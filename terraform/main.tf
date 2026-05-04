a006cb88da77259345687de8608802ab7ed7bcc8
manar <manar@gmail.com>
2026-05-04 20:15:22 +0300

    Add http rule on port 80 instead of 5000

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,15 +23,8 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5001
-    to_port     = 5001
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
